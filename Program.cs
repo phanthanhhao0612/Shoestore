d39db8f4c7456e0a748b5e47ee98ea58db3ed228
phanthanhhao0612 <148755127+phanthanhhao0612@users.noreply.github.com>
2025-08-05 21:45:58 +0700

    Update docker configuration and add new docker compose files

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ using Microsoft.IdentityModel.Tokens;
 using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
-builder.WebHost.UseUrls("http://0.0.0.0:91");
+builder.WebHost.UseUrls("http://0.0.0.0:8080");
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
